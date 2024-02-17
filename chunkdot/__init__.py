113dbf4575d7774b37160dce20ccfdb7a3f4a06a
Rodrigo Agundez <rragundez@gmail.com>
2024-02-17 11:18:43 +0800

    Add helpful message for importing the transformer

--- chunkdot/__init__.py
+++ chunkdot/__init__.py
@@ -5,10 +5,15 @@ from chunkdot.cosine_similarity_top_k import cosine_similarity_top_k
 from chunkdot.utils import is_package_installed
 
 if is_package_installed("sklearn"):
+    # Scikit-learn is a big package and since it is only necessary if using the transformer
+    # CosineSimilarityTopK it is not included as an explicit dependency. Therefore, the
+    # transformer CosineSimilarityTopK is only loaded if sklearn is already installed by the user.
     from chunkdot.cosine_similarity_top_k import CosineSimilarityTopK
 else:
     warnings.warn(
-        "Scikit-Learn not installed. Not importing the ChunkDot Transformer 'CosineSimilarityTopK'"
+        "Scikit-learn is not installed, therefore chunkdot's sklearn transformer 'CosineSimilarityTopK' "
+        "will not be loaded. If you plan to use the 'CosineSimilarityTopK' transformer please install "
+        "scikit-learn and re-import chunkdot."
     )
 
 __version__ = "0.3.0"
